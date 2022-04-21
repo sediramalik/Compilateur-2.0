ca119980ae08b2b6882acb332d99f27326971216
Rostom Baccar <81230614+rostom-baccar@users.noreply.github.com>
2022-04-21 12:28:53 +0200

    files upload error fixed

---
+++ Microprocessor/Microprocessor.srcs/sources_1/new/REGISTER_FILE.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.04.2022 11:28:45
+-- Design Name: 
+-- Module Name: REGISTER_FILE - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity REGISTER_FILE is
+    Port ( aA : in STD_LOGIC_VECTOR (3 downto 0);
+           aB : in STD_LOGIC_VECTOR (3 downto 0);
+           aW : in STD_LOGIC_VECTOR (3 downto 0);
+           W : in STD_LOGIC;
+           DATA : in STD_LOGIC_VECTOR (7 downto 0);
+           RST : in STD_LOGIC;
+           CLK : in STD_LOGIC;
+           QA : out STD_LOGIC_VECTOR (7 downto 0);
+           QB : out STD_LOGIC_VECTOR (7 downto 0));
+end REGISTER_FILE;
+
+architecture Behavioral of REGISTER_FILE is
+
+begin
+
+process (CLK)
+    begin
+    if CLK ='1' then  
+    end process;
+
+
+end Behavioral;
--- Microprocessor/Microprocessor.srcs/sources_1/new/UAL.vhd
+++ Microprocessor/Microprocessor.srcs/sources_1/new/UAL.vhd
@@ -50,16 +50,17 @@ signal OP : STD_LOGIC_VECTOR (15 downto 0);
 begin 
 process(A,B,CTRL_ALU)
     begin
-        if CTRL_ALU="01" then OP<=(x"00"&A) + (x"00"&B); --OP ADD
-        elsif CTRL_ALU="11" then OP<=(x"00"&A) - (x"00"&B); --OP SUB
-        elsif CTRL_ALU="10" then OP<= A * B; --OP MUL
+        if CTRL_ALU="001" then OP<=(x"00"&A) + (x"00"&B); --OP ADD
+        elsif CTRL_ALU="011" then OP<=(x"00"&A) - (x"00"&B); --OP SUB
+        elsif CTRL_ALU="010" then OP<= A * B; --OP MUL
+        elsif CTRL_ALU="100" then OP<= shr(A,"1"); --OP DIV (ONLY BY 2 : LEFT SHIFT)
     end if;
 end process;
 
 --UPDATE FLAGS
 N <= '1' when OP < 0 else '0'; --If the result if negative the flag is updated  
+Z <= '1' when OP(15 downto 0) = x"00" else '0'; 
 O <= '1' when OP(15 downto 8) /=x"00" else '0'; --We check if bits from 8 to 15 have been changed
-Z <= '1' when OP(15 downto 0) = (others => '0') else '0'; 
 C <= OP(8); --Carry of OP ADD in the 8th bit
 
 S <= OP (7 downto 0); --Output stored
