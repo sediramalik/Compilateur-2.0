0c746025e9b23a40d63958b1dca918c9e1bdd1de
Rostom Baccar <81230614+rostom-baccar@users.noreply.github.com>
2022-06-02 04:59:56 +0200

    Added final test image

--- Compiler/Everything.c
+++ Compiler/Everything.c
@@ -1,42 +1,32 @@
 void main()
 {
-
     int a = 10;
     const int b = 20;
     int c;
     c = 30;
     const int d;
     d = 40;
-
     int e;
     e = a + b - c * d;
-
-    if (a)
-    {
+    if (a){
         fun1(b);
         print(c);
     }
-
-    if (a == b)
-    {
+    if (a == b){
         int la = a;
         int lb = b;
         int lc;
         lc = la + lb;
         print(lc);
     }
-    else
-    {
+    else{
         print(e);
         fun2(e, d);
     }
-
-    while (a)
-    {
+    while (a){
         print(c);
     }
-    while (a > b + c)
-    {
+    while (a > b + c){
         int ld = 50;
         int le;
         int lf;
@@ -44,21 +34,18 @@ void main()
         lf = fun4(a, b, c, d, e) - le + ld;
     }
 }
-
 void fun1(int x) { print(x); }
-void fun2(int x, int y)
-{
+void fun2(int x, int y){
     y = x;
     print(x);
 }
-int fun3(int x)
-{
+int fun3(int x){
     x = x + 1;
     return x;
 }
-int fun4(int a, int b, int c, int d, int e)
-{
+int fun4(int a, int b, int c, int d, int e){
     const int result;
     result = a + b - c * d / e;
     return result;
 }
+S
